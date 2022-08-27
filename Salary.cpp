351780197e83d183f824e123f9999ecc206db80d
Mahmudul Hasan Sarkar <71069222+Mahmud256@users.noreply.github.com>
2022-08-27 11:36:26 +0600

    Create Salary.cpp

---
+++ Salary.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float A, B, C;
+   cin>>A>>B>>C;
+
+   cout<<"NUMBER = "<<A<<endl;
+   printf("SALARY = U$ %.2lf\n",B*C);
+
+   return 0;
+}
+
+
+
