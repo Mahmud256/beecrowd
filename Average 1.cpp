9d95cb216cb9bdf35e5b9068dc3d5a57f8a61b33
Mahmudul Hasan Sarkar <71069222+Mahmud256@users.noreply.github.com>
2022-08-27 10:54:28 +0600

    Create Average 1.cpp

---
+++ Average 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float A, B;
+   cin>>A>>B;
+
+   float MEDIA = ((A*3.5)+(B*7.5))/(3.5+7.5);
+
+   printf("MEDIA = %.5lf\n",MEDIA);
+
+   return 0;
+}
