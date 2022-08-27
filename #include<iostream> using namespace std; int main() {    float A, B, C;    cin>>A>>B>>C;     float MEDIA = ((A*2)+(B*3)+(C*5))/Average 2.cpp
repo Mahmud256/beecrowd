298c9d0e674f38278bde28f0b4867de66f20f12a
Mahmudul Hasan Sarkar <71069222+Mahmud256@users.noreply.github.com>
2022-08-27 11:17:05 +0600

    Create Average 2.cpp

---
+++ #include<iostream> using namespace std; int main() {    float A, B, C;    cin>>A>>B>>C;     float MEDIA = ((A*2)+(B*3)+(C*5))/Average 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float A, B, C;
+   cin>>A>>B>>C;
+
+   float MEDIA = ((A*2)+(B*3)+(C*5))/(2+3+5);
+
+   printf("MEDIA = %.1lf\n",MEDIA);
+
+   return 0;
+}
+
